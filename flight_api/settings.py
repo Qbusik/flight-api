cb3528a00a436a20fd4da15bee6dd61e2b537b82
Jakub Paluch <qbusik@gmail.com>
2026-02-04 09:31:34 +0100

    add throttling to settings.py

--- flight_api/settings.py
+++ flight_api/settings.py
@@ -130,4 +130,14 @@ INTERNAL_IPS = [
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle"
+    ],
+
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "100/day",
+        "user": "1000/day"
+    }
 }
